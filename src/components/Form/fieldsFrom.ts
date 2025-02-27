39ca5284fd9ad9800bdc3d167c181a23b04a6833
Adrian <agyaniri@gmail.com>
2025-02-27 13:41:45 -0300

    Refactorizacion del componente form

--- src/components/Form/fieldsFrom.ts
+++ src/components/Form/fieldsFrom.ts
@@ -1,9 +1,50 @@
-const fields = [
-  { id: "nombre", label: "Nombre", type: "text", placeholder: "Tu nombre" },
-  { id: "empresa", label: "Empresa", type: "text", placeholder: "Tu empresa" },
-  { id: "email", label: "Correo Electrónico", type: "email", placeholder: "Tu correo" },
-  { id: "telefono", label: "Teléfono", type: "tel", placeholder: "Tu teléfono" },
+export type Field = {
+  id: string; 
+  label: string;
+  type:  'text' | 'email' | 'tel' | 'textarea';
+  placeholder: string;
+  required?: boolean;
+  validationRule?: RegExp;
+}
+
+export const fields: Field[] = [
+  {
+    id: 'name',
+    label: 'Nombre',
+    type: 'text',
+    placeholder: 'Escribe tu nombre',
+    required: true,
+    validationRule: /^[a-zA-Z\s]+$/,
+  },
+  {
+    id: 'empresa',
+    label: 'Empresa',
+    type: 'text',
+    placeholder: 'Escribe el nombre de tu empresa',
+    required: true,
+    validationRule: /^[a-zA-Z\s]+$/,
+  },
+  {
+    id: 'email',
+    label: 'Correo Electrónico',
+    type: 'email',
+    placeholder: 'ejemplo@correo.com',
+    required: true,
+    validationRule: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  {
+    id: 'telefono',
+    label: 'Teléfono',
+    type: 'tel',
+    placeholder: 'Escribe tu número de teléfono',
+    required: true,
+  },
+  {
+    id: 'mensaje',
+    label: 'Mensaje',
+    type: 'textarea',
+    placeholder: 'Escribe tu mensaje aquí...',
+    required: false,
+  },
 
 ];
-
-export default fields;
