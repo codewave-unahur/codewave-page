6d763be94bffa8a6ec36a0e36742f39ea59335d9
Adrian <agyaniri@gmail.com>
2025-03-05 11:00:39 -0300

    configuracion para produccion

--- src/components/Contacto/Contacto.tsx
+++ src/components/Contacto/Contacto.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Section } from "../common/Secciones";
 import { FadeInSection } from "../common/FadeInSection";
 import FormFields from "../Form/FormFields";
 import SubmitButton from "../Form/SubmitButton";
 import { useForm } from "../Form/useForm";
 
-
 export const Contacto: React.FC = () => {
-  const { formData, errors, submitStatus, isSubmitting, handleChange, handleSubmit } = useForm();
+  const { formData, errors, submitStatus, isSubmitting, handleChange, handleSubmit, clearSubmitStatus } = useForm();
+
+  useEffect(() => {
+    if (submitStatus) {
+      const timer = setTimeout(() => {
+        clearSubmitStatus();
+      }, 5000); // El mensaje desaparecerá después de 5 segundos
+
+      return () => clearTimeout(timer);
+    }
+  }, [submitStatus, clearSubmitStatus]);
 
   return (
     <FadeInSection>
